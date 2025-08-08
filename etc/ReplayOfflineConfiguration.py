b2edd649e9dff55e147f5772d37c2be18d21b265
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2025-08-08 11:30:24 +0200

    Replay configuration for CMSSW_15_0_12

--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -42,6 +42,7 @@ setConfigVersion(tier0Config, "replace with real version")
 # 386674  Cosmics ~40 minutes in Run2024I with occupancy issues
 
 setInjectRuns(tier0Config, [393274]) # 386925: 2024 Collisions, 390094: 2025 Cosmics, 390951: 2025 900 GeV Collisions
+setInjectRuns(tier0Config, [390094]) # 386925: 2024 Collisions, 390094: 2025 Cosmics, 390951: 2025 900 GeV Collisions
 
 # Use this in order to limit the number of lumisections to process
 #setInjectLimit(tier0Config, 10)
@@ -147,7 +148,7 @@ setPromptCalibrationConfig(tier0Config,
 
 # Defaults for CMSSW version
 defaultCMSSWVersion = {
-    'default': "CMSSW_15_0_10_patch1"
+    'default': "CMSSW_15_0_12"
 }
 
 # Configure ScramArch
